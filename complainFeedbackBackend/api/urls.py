907c4c8f128d753d6678d8a90c1516484e8683cf
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2025-08-11 07:41:47 +0545

    first commit

---
+++ complainFeedbackBackend/api/urls.py
@@ -0,0 +1,11 @@
+# api/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ComplainViewSet
+
+router = DefaultRouter()
+router.register(r'complains', ComplainViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),  # Removed 'api/'
+]
