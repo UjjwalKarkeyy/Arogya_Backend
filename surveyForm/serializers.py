369fbe9becdcfad04023393a4db8d62e58c0cbdc
Dhijan <dahaldhijan@gmail.com>
2025-08-19 17:43:58 +0545

    update the api routing

--- surveyForm/serializers.py
+++ surveyForm/serializers.py
@@ -4,13 +4,43 @@ from .models import Survey, Question
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = '__all__'
+        # Expose key fields; survey will be set by parent serializer on create
+        fields = (
+            'id',
+            'title',
+            'description',
+            'question_type',
+            'required',
+        )
 
 
 class SurveySerializer(serializers.ModelSerializer):
     # include questions inside survey
-    questions = QuestionSerializer(many=True, read_only=True)
+    questions = QuestionSerializer(many=True, required=False)
 
     class Meta:
         model = Survey
-        fields = '__all__'
+        fields = 'all'
+
+    def create(self, validated_data):
+        questions_data = validated_data.pop('questions', [])
+        survey = Survey.objects.create(validated_data)
+        for q in questions_data:
+            Question.objects.create(survey=survey, q)
+        return survey
+
+    def update(self, instance, validated_data):
+        questions_data = validated_data.pop('questions', None)
+
+        # Update survey fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        # If questions provided, replace existing questions with new set
+        if questions_data is not None:
+            instance.questions.all().delete()
+            for q in questions_data:
+                Question.objects.create(survey=instance, **q)
+
+        return instance
